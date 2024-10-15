52e36a7432ae52aac65d85cc684048f31fe909f3
redcliff-op <devyashjangid@gmail.com>
2024-10-15 15:56:41 +0530

    Cors: Allow localhost for testing

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ import cookieParser from 'cookie-parser';
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.DEV_FRONTEND_URI,
+  credentials:true
+}))
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(express.static('public'))
